34dc628dd969970dbd97bde9c6ed8672608ceac6
juanfelipe <maflay26@gmail.com>
2025-04-23 16:58:03 -0500

    primer commit

---
+++ functions.js
@@ -0,0 +1,7 @@
+// let index = 1;
+//   const total = 5;
+
+//   setInterval(() => {
+//     document.getElementById(`radio${index}`).checked = true;
+//     index = index % total + 1;
+//   }, 3000);
