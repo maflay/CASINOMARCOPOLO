b6161834fd0aa5a9a9a858d8d079f0b9829074ad
juanfelipe <maflay26@gmail.com>
2025-05-16 17:57:40 -0500

    contenido, cambios de obtener tarjeta

--- components/home/home.js
+++ components/home/home.js
@@ -12,124 +12,125 @@ function saberMasPromo() {
 }
 
 function asociarse() {
-  navegarA("asociado")
+  navegarA("asociado");
   setTimeout(() => {
     window.scrollTo({ top: 0, behavior: "auto" });
   }, 300);
 }
 
-function toprogresivos(){
+function toprogresivos() {
   navegarA("clubnavegante?id=section-progresivos");
 }
 
+// // Slider Principal
+// const track = document.getElementById("sliderTrack");
+// const dots = document.querySelectorAll(".dot");
+// const prevBtn = document.getElementById("prevBtn");
+// const nextBtn = document.getElementById("nextBtn");
 
+// let currentIndex = 0;
+// const totalSlides = dots.length;
+// let interval;
 
-    // // Slider Principal
-    // const track = document.getElementById("sliderTrack");
-    // const dots = document.querySelectorAll(".dot");
-    // const prevBtn = document.getElementById("prevBtn");
-    // const nextBtn = document.getElementById("nextBtn");
+// function goToSlide(index) {
+//   track.style.transform = `translateX(-${index * 100}vw)`;
+//   dots.forEach(dot => dot.classList.remove("active"));
+//   dots[index].classList.add("active");
+//   currentIndex = index;
+// }
 
-    // let currentIndex = 0;
-    // const totalSlides = dots.length;
-    // let interval;
+// function nextSlide() {
+//   let index = (currentIndex + 1) % totalSlides;
+//   goToSlide(index);
+// }
 
-    // function goToSlide(index) {
-    //   track.style.transform = `translateX(-${index * 100}vw)`;
-    //   dots.forEach(dot => dot.classList.remove("active"));
-    //   dots[index].classList.add("active");
-    //   currentIndex = index;
-    // }
+// function prevSlide() {
+//   let index = (currentIndex - 1 + totalSlides) % totalSlides;
+//   goToSlide(index);
+// }
 
-    // function nextSlide() {
-    //   let index = (currentIndex + 1) % totalSlides;
-    //   goToSlide(index);
-    // }
+// function resetInterval() {
+//   clearInterval(interval);
+//   interval = setInterval(nextSlide, 5000);
+// }
 
-    // function prevSlide() {
-    //   let index = (currentIndex - 1 + totalSlides) % totalSlides;
-    //   goToSlide(index);
-    // }
+// // Botones
+// nextBtn.addEventListener("click", () => {
+//   nextSlide();
+//   resetInterval();
+// });
 
-    // function resetInterval() {
-    //   clearInterval(interval);
-    //   interval = setInterval(nextSlide, 5000);
-    // }
+// prevBtn.addEventListener("click", () => {
+//   prevSlide();
+//   resetInterval();
+// });
 
-    // // Botones
-    // nextBtn.addEventListener("click", () => {
-    //   nextSlide();
-    //   resetInterval();
-    // });
+// // Dots
+// dots.forEach(dot => {
+//   dot.addEventListener("click", () => {
+//     let index = parseInt(dot.dataset.index);
+//     goToSlide(index);
+//     resetInterval();
+//   });
+// });
 
-    // prevBtn.addEventListener("click", () => {
-    //   prevSlide();
-    //   resetInterval();
-    // });
+// // Auto Slide
+// interval = setInterval(nextSlide, 5000);
 
-    // // Dots
-    // dots.forEach(dot => {
-    //   dot.addEventListener("click", () => {
-    //     let index = parseInt(dot.dataset.index);
-    //     goToSlide(index);
-    //     resetInterval();
-    //   });
-    // });
+// Slider Muestras
+function slidermuestra() {
+  const trackMuestras = document.getElementById("sliderTrackMuestras");
+  const radiosMuestras = document.querySelectorAll(
+    'input[name="slider-muestras"]'
+  );
+  const labelsMuestras = document.querySelectorAll(
+    ".navigation-muestras label"
+  );
+  const prevBtnMuestra = document.getElementById("prevBtnMuestra");
+  const nextBtnMuestra = document.getElementById("nextBtnMuestra");
 
-    // // Auto Slide
-    // interval = setInterval(nextSlide, 5000);
+  let currentIndexMuestra = 0;
+  const totalSlidesMuestra = radiosMuestras.length;
+  let intervalMuestra;
 
+  function goToSlideMuestra(index) {
+    trackMuestras.style.transform = `translateX(-${index * 100}vw)`;
+    radiosMuestras[index].checked = true;
+    currentIndexMuestra = index;
+  }
 
-    // Slider Muestras
-    function slidermuestra(){
+  function nextSlideMuestra() {
+    let index = (currentIndexMuestra + 1) % totalSlidesMuestra;
+    goToSlideMuestra(index);
+  }
 
-    const trackMuestras = document.getElementById("sliderTrackMuestras");
-    const radiosMuestras = document.querySelectorAll('input[name="slider-muestras"]');
-    const labelsMuestras = document.querySelectorAll(".navigation-muestras label");
-    const prevBtnMuestra = document.getElementById("prevBtnMuestra");
-    const nextBtnMuestra = document.getElementById("nextBtnMuestra");
+  function prevSlideMuestra() {
+    let index =
+      (currentIndexMuestra - 1 + totalSlidesMuestra) % totalSlidesMuestra;
+    goToSlideMuestra(index);
+  }
 
-    let currentIndexMuestra = 0;
-    const totalSlidesMuestra = radiosMuestras.length;
-    let intervalMuestra;
-
-    function goToSlideMuestra(index) {
-      trackMuestras.style.transform = `translateX(-${index * 100}vw)`;
-      radiosMuestras[index].checked = true;
-      currentIndexMuestra = index;
-    }
-
-    function nextSlideMuestra() {
-      let index = (currentIndexMuestra + 1) % totalSlidesMuestra;
-      goToSlideMuestra(index);
-    }
-
-    function prevSlideMuestra() {
-      let index = (currentIndexMuestra - 1 + totalSlidesMuestra) % totalSlidesMuestra;
-      goToSlideMuestra(index);
-    }
-
-    function resetIntervalMuestra() {
-      clearInterval(intervalMuestra);
-      intervalMuestra = setInterval(nextSlideMuestra, 3000);
-    }
-
-    nextBtnMuestra.addEventListener("click", () => {
-      nextSlideMuestra();
-      resetIntervalMuestra();
-    });
-
-    prevBtnMuestra.addEventListener("click", () => {
-      prevSlideMuestra();
-      resetIntervalMuestra();
-    });
-
-    labelsMuestras.forEach((label, index) => {
-      label.addEventListener("click", () => {
-        goToSlideMuestra(index);
-        resetIntervalMuestra();
-      });
-    });
+  function resetIntervalMuestra() {
+    clearInterval(intervalMuestra);
     intervalMuestra = setInterval(nextSlideMuestra, 3000);
-    }
+  }
+
+  nextBtnMuestra.addEventListener("click", () => {
+    nextSlideMuestra();
+    resetIntervalMuestra();
+  });
+
+  prevBtnMuestra.addEventListener("click", () => {
+    prevSlideMuestra();
+    resetIntervalMuestra();
+  });
+
+  labelsMuestras.forEach((label, index) => {
+    label.addEventListener("click", () => {
+      goToSlideMuestra(index);
+      resetIntervalMuestra();
+    });
+  });
+  intervalMuestra = setInterval(nextSlideMuestra, 3000);
+}
 slidermuestra();
