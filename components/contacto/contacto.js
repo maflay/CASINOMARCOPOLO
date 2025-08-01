52ab069dd222cc19e1c2353638ed06b5fe60f825
juanfelipe <maflay26@gmail.com>
2025-08-01 10:20:44 -0500

    guardar info contacto

--- components/contacto/contacto.js
+++ components/contacto/contacto.js
@@ -51,31 +51,85 @@ function infoSendContacto() {
   const telefono = document.getElementById("telefono");
   const ciudad = document.getElementById("ciudad");
   const direccion = document.getElementById("direccion");
-  const fecha_solicitud = document.getElementById("fecha_solicitud");
   const descripcion = document.getElementById("descripcion");
   const es_cliente = document.getElementById("es_cliente");
   const aceptoTerminos = document.getElementById("aceptoTerminos");
+  const loader = document.getElementById("loading");
 
   const nombreEn = nombre.value;
   const correoEn = correo.value;
   const telefonoEn = telefono.value;
   const ciudadEn = ciudad.value;
   const direccionEn = direccion.value;
-  const fecha_solicitudEn = fecha_solicitud.value;
   const descripcionEn = descripcion.value;
   const es_clienteEn = es_cliente.value;
   const aceptoTerminosEn = aceptoTerminos.value;
 
+  const url =
+    "https://script.google.com/macros/s/AKfycbwatZBZdXwAjc1VkNqywr4CENX7ipZA8T1GHn3u6PQYoQu6YDKbRnz0PleJrtiDtSsA/exec";
+  loader.style.display = "flex";
+
+
+    const fechaCompleta = new Date().toLocaleString("es-CO", {
+    timeZone: "America/Bogota",
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+  });
+
+  const [fecha, hora] = fechaCompleta.split(", ");
+
+
   const data = {
-    nombreEn,
-    correoEn,
-    telefonoEn,
-    ciudadEn,
-    direccionEn,
-    fecha_solicitudEn,
-    descripcionEn,
-    es_clienteEn,
-    aceptoTerminosEn,
+    nombre: nombreEn,
+    correo: correoEn,
+    telefono: telefonoEn,
+    ciudad: ciudadEn,
+    direccion: direccionEn,
+    fechasoli: fecha,
+    descripcion: descripcionEn,
+    cliente: es_clienteEn,
+    terminos: aceptoTerminosEn,
   };
 
+  console.log(data);
+
+  fetch(url, {
+    method: "POST",
+    mode: "no-cors",
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.text())
+    .then(() => {
+
+      nombre.value = "";
+      correo.value = "";
+      telefono.value = "";
+      ciudad.value = "";
+      direccion.value = "";
+      descripcion.value = "";
+      es_cliente.checked = false;
+      aceptoTerminos.checked = false;
+
+      setTimeout(() => {
+        loader.style.display = "none";
+          Swal.fire({
+          icon: "success",
+          title: "EXITO",
+          text: "El envio de la información fue exitoso.",
+          confirmButtonColor: "#1F253A",
+           customClass: {
+            popup: "mi-popup",
+            title: "mi-titulo",
+            confirmButton: "btn-Send mi-boton",
+          },
+        })
+      }, 2000);
+    })
+    .catch((error) => {
+      loader.style.display = "none";
+    });
 }
