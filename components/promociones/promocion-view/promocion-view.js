40ccd69a976ed4d3a9f6548f97fb756faeabe208
juanfelipe <maflay26@gmail.com>
2025-11-24 11:13:59 -0500

    cambios, promos diciembre

--- components/promociones/promocion-view/promocion-view.js
+++ components/promociones/promocion-view/promocion-view.js
@@ -6,10 +6,12 @@
   const cubo1 = document.getElementById("cubo1-mp");
   const cubo2 = document.getElementById("cubo2-mp");
   const cubo3 = document.getElementById("cubo3-mp");
+  const cubo4 = document.getElementById("cubo4-mp");
 
   cubo1.style.display = "none";
   cubo2.style.display = "none";
   cubo3.style.display = "none";
+  cubo4.style.display = "none";
 
   if (solofechaCompleta == "septiembre") {
     cubo1.style.display = "flex";
@@ -17,6 +19,8 @@
     cubo2.style.display = "flex";
   } else if (solofechaCompleta == "noviembre") {
     cubo3.style.display = "flex";
+  } else if (solofechaCompleta == "diciembre") {
+    cubo4.style.display = "flex";
   }
 })();
 
