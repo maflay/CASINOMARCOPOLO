57b3b19c2ce7f676f1ccf3f02695036604283861
juanfelipe <maflay26@gmail.com>
2025-12-05 15:52:07 -0500

    cambios

--- components/promociones/promocion/tren-premios.js
+++ components/promociones/promocion/tren-premios.js
@@ -1,14 +1,3 @@
-(() => {
-  const btnToBlackJackExpress = document.getElementById(
-    "next-to-blackjack-express"
-  );
-
-  btnToBlackJackExpress.addEventListener("click", () => {
-    navegarA("blackjackexpress");
-  });
-  
-})();
-
 function toPromos(){
   navegarA("promociones");
 }
