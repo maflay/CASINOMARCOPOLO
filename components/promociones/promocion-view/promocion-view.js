d322c540796e5c42f7d450c25e9576ea3a1fcab2
juanfelipe <maflay26@gmail.com>
2026-08-04 09:18:15 -0500

    ultimos cambios

--- components/promociones/promocion-view/promocion-view.js
+++ components/promociones/promocion-view/promocion-view.js
@@ -9,15 +9,7 @@
   const promociones_abril = document.getElementById("promociones_abril");
   const promociones_mayo = document.getElementById("promociones_mayo");
   const promociones_junio = document.getElementById("promociones_junio");
-  const cubo1 = document.getElementById("cubo1-mp");
-  const cubo2 = document.getElementById("cubo2-mp");
-  const cubo4 = document.getElementById("cubo4-mp");
-
-  promociones_enero.style.display = "none";
-  promociones_febrero.style.display = "none";
-  cubo1.style.display = "none";
-  cubo2.style.display = "none";
-  cubo4.style.display = "none";
+  const promociones_agosto = document.getElementById("promociones_agosto");
 
   if (solofechaCompleta == "enero") {
     promociones_enero.style.display = "flex";
@@ -47,31 +39,9 @@
     promociones_junio.style.display = "flex";
   }
 
-  if (solofechaCompleta == "septiembre") {
-    cubo1.style.display = "flex";
+  if(solofechaCompleta == "agosto"){
+    promociones_agosto.style.display = "flex";
   }
-
-  if (solofechaCompleta == "octubre") {
-    cubo2.style.display = "flex";
-  }
-
-  if (solofechaCompleta == "diciembre") {
-    cubo4.style.display = "flex";
-  }
-})();
-
-(() => {
-  const allitem = document.querySelectorAll(".card-content p");
-
-  allitem.forEach((item) => {
-    let item_len = item.textContent;
-    let long_item = item_len.length;
-    let end_long;
-    if (long_item > 150) {
-      end_long = item_len.slice(0, 155) + "...";
-      item.textContent = end_long;
-    }
-  });
 })();
 
 (() => {
